029ff56d82ce59ea91162ed882c5d4dde14211c7
LuispeFigueroa <fig24087@uvg.edu.gt>
2026-05-01 00:39:14 -0600

    CRUD divisiones

--- handlers/divisiones.go
+++ handlers/divisiones.go
@@ -29,7 +29,7 @@ func GetDivisiones(c *gin.Context) {
 	c.JSON(http.StatusOK, divisiones)
 }
 
-// Get divisiones por id /divisiones/id
+// Get divisiones por id /divisiones/:id
 func GetDivision(c *gin.Context) {
 	id := c.Param("id")
 	var d models.Division
@@ -44,7 +44,7 @@ func GetDivision(c *gin.Context) {
 	c.JSON(http.StatusOK, d)
 }
 
-//POST para agregar una divisio /divisiones
+//POST para agregar una division /divisiones
 
 func CreateDivision(c *gin.Context) {
 	var d models.Division
@@ -64,4 +64,49 @@ func CreateDivision(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusCreated, d)
+}
+
+// PUT  para modificar una division especificia/divisiones/:id
+func UpdateDivision(c *gin.Context) {
+	id := c.Param("id")
+	var d models.Division
+
+	if err := c.ShouldBindJSON(&d); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Datos no validos"})
+		return
+	}
+
+	result, err := DB.Exec(
+		"UPDATE divisiones SET nombre=$1, temporada$=2 WHERE id=$3",
+		d.Nombre, d.Temporada, id,
+	)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error al actualizar la division"})
+		return
+	}
+
+	rowsAffected, _ := result.RowsAffected()
+	if rowsAffected == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "No se encontro a la divisoin"})
+		return
+	}
+	d.ID = 0
+	c.JSON(http.StatusOK, d)
+}
+
+// Delete de una division especifica /divisiones/:id
+func DeleteDivision(c *gin.Context) {
+	id := c.Param("id")
+
+	result, err := DB.Exec("DELETE FROM divisiones WHERE id=$1", id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "error al eliminar la division"})
+		return
+	}
+	rowsAffected, _ := result.RowsAffected()
+	if rowsAffected == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Division no encontrada"})
+	}
+	c.JSON(http.StatusNotFound, nil)
 }
